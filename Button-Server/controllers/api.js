b229cc548043eaac9c1d90291908660ddee1b622
Stephen Greco <stephen@sgre.co>
2014-04-01 17:59:59 -0400

    fixed post requests

--- Button-Server/controllers/api.js
+++ Button-Server/controllers/api.js
@@ -3,29 +3,31 @@ var Coord = require('mongoose').model('Coord');
 module.exports = function(app) {
 
 	app.get('/api/sendLocation', function(req,res){
+	});
+
+	app.post('/api/sendLocation', function(req,res){
+		console.log(req.body);
 		if (req.headers["user-agent"].indexOf("Button") < 0){
 			return res.json({
 				status: 403,
 				message: "Device forbidden."
 			});
 		}
-		else if (!req.query.idnum){
+		else if (!req.body.idnum){
 			return res.json({
 				status: 500,
 				message: "No identifier provided."
 			});
 		}
-		else if (!req.query.lat || !req.query.lng){
+		else if (!req.body.lat || !req.body.lng){
 			return res.json({
 				status: 500,
 				message: "Please provide valid coordinates."
 			});
 		}
-		var lat = parseFloat(req.query.lat);
-		var lon = parseFloat(req.query.lng);
 		Coord.findOneAndUpdate(
-			{identifier : req.query.idnum},
-			{coords : [lon, lat],
+			{identifier : req.body.idnum},
+			{coords : [req.body.lng, req.body.lat],
 			timestamp : new Date()},
 			{upsert : true},
 			function (err){
@@ -39,21 +41,6 @@ module.exports = function(app) {
 					status: 200,
 					message: "Location successfully saved."
 				});
-			});
-		/*var boner = new Coord(data);
-		boner.save(function(err){
-			if (err){
-				return res.json({
-					status: 500,
-					message: "There was an error saving coordinates."
-				});
-			}
-			else return res.json({
-				status: 200,
-				message: "Location successfully saved."
-			});
 		});
-		*/
 	});
-
 };
